c59748dba4058a7ac1eff39cf4274882d6f275ae
radmirnovii <radmirnovii@gmail.com>
2017-04-11 03:22:28 +0400

    добавил контроллер и форму

---
+++ src/main/java/com/dosug/app/controller/RegistrationController.java
@@ -0,0 +1,55 @@
+package com.dosug.app.controller;
+
+import com.dosug.app.domain.User;
+import com.dosug.app.form.RegistrationForm;
+import com.dosug.app.respose.model.ApiError;
+import com.dosug.app.respose.model.Response;
+import com.dosug.app.services.RegistrationService;
+import com.dosug.app.services.validation.ValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author radmirnovii
+ */
+@RestController
+public class RegistrationController {
+
+    private RegistrationService registrationService;
+
+    private ValidationService validationService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Response register(@RequestBody RegistrationForm form) {
+        Response<Void> response = new Response<>();
+
+        List<ApiError> validationErrors = validationService.validate(form);
+        if( !validationErrors.isEmpty()) {
+            return response.failure(validationErrors);
+        }
+
+        User user = new User(form.getUsername(), form.getEmail(), form.getPassword());
+
+        List<ApiError> serviceErrors = registrationService.registration(user);
+        if( !serviceErrors.isEmpty()) {
+            return response.failure(serviceErrors);
+        }
+
+        return response.success(null);
+    }
+
+    @Autowired
+    public void setRegistrationService(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @Autowired
+    public void setValidationService(ValidationService validationService) {
+        this.validationService = validationService;
+    }
+}
---
+++ src/main/java/com/dosug/app/form/RegistrationForm.java
@@ -0,0 +1,80 @@
+package com.dosug.app.form;
+
+import com.dosug.app.respose.model.ApiErrorCode;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+/**
+ * Форма для регистрации
+ */
+public class RegistrationForm {
+    private static final int EMAIL_MAX_LENGTH = 256;
+    private static final int MAX_PASSWORD_LENGTH = 256;
+    private static final int USERNAME_MAX_LENGTH = 256;
+
+    @ErrorCode(code = ApiErrorCode.INVALID_USERNAME)
+    @Length(min = 1, max = USERNAME_MAX_LENGTH, message = "username length from 1 to 256")
+    @NotNull(message = "username field is required")
+    @Pattern(regexp = "[a-zA-Z0-9-_]*", message = "only latin character, digits, underline and hyphen allowed in username")
+    private String username;
+
+    @ErrorCode(code = ApiErrorCode.INVALID_EMAIL)
+    @NotNull(message = "email field is required")
+    @Size(min = 1, max = EMAIL_MAX_LENGTH, message = "email length from 1 to 256")
+    @Email(message = "not correct email")
+    private String email;
+
+    @ErrorCode(code = ApiErrorCode.INVALID_PASSWORD)
+    @NotNull(message = "password field is required")
+    @Size(min = 1, max = MAX_PASSWORD_LENGTH, message = "password length from 1 to 256")
+    @Pattern(regexp = "[a-zA-Z0-9_]*", message = "only latin character, digits and underscore allowed in password")
+    private String password;
+
+    private String passwordRetry;
+
+    @ErrorCode(code = ApiErrorCode.INVALID_PASSWORD_RETRY)
+    @AssertTrue(message = "password and password retry not equals")
+    public boolean isRetryEqualsPassword() {
+        // NPE check
+        if (password != null) {
+            return password.equals(passwordRetry);
+        }
+        return false;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordRetry() {
+        return passwordRetry;
+    }
+
+    public void setPasswordRetry(String passwordRetry) {
+        this.passwordRetry = passwordRetry;
+    }
+}
