95f38f41db53fad72d076eeaf724c628fc51b5b1
radmirnovii <radmirnovii@gmail.com>
2017-04-11 03:21:03 +0400

    новые коды ошибок

--- src/main/java/com/dosug/app/respose/model/ApiErrorCode.java
+++ src/main/java/com/dosug/app/respose/model/ApiErrorCode.java
@@ -7,9 +7,16 @@ import com.fasterxml.jackson.annotation.JsonValue;
  */
 public enum ApiErrorCode {
 
+    UNKNOWN_SERVICE_PROBLEM(0),
     WRONG_LOGIN_AND_PASSWORD(1),
     INVALID_USERNAME(2),
-    INVALID_PASSWORD(3);
+    INVALID_PASSWORD(3),
+    INVALID_EMAIL(4),
+    INVALID_PASSWORD_RETRY(5),
+
+    USERNAME_ALREADY_USE(6),
+    EMAIL_ALREADY_USE(7);
+
 
     private int errorCode;
 
