00f54b759d6487c4b2065405c9873739248dc51c
radmirnovii <radmirnovii@gmail.com>
2017-04-12 12:11:32 +0400

    изменил url

--- src/main/java/com/dosug/app/controller/RegistrationController.java
+++ src/main/java/com/dosug/app/controller/RegistrationController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,6 +19,7 @@ import java.util.List;
  * @author radmirnovii
  */
 @RestController
+@RequestMapping(value = "/registration")
 public class RegistrationController {
 
     private RegistrationService registrationService;
