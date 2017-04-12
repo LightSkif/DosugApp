b868e0acd2898224786cf07298873dbcef6ce398
radmirnovii <radmirnovii@gmail.com>
2017-04-12 12:36:37 +0400

    добавил дату создания

--- src/main/java/com/dosug/app/services/SimpleRegistrationService.java
+++ src/main/java/com/dosug/app/services/SimpleRegistrationService.java
@@ -7,6 +7,7 @@ import com.dosug.app.respose.model.ApiErrorCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -38,6 +39,9 @@ public class SimpleRegistrationService implements RegistrationService {
         }
 
         format(user);
+
+        //дата создания
+        user.setCreateDate(LocalDateTime.now());
 
         try {
             userRepository.save(user);
