8ffb71b341b4b600e1e1790658558f7ba3705610
radmirnovii <radmirnovii@gmail.com>
2017-04-17 21:04:48 +0400

    добавил метод аутентификации

--- src/main/java/com/dosug/app/services/authentication/AuthenticationService.java
+++ src/main/java/com/dosug/app/services/authentication/AuthenticationService.java
@@ -1,6 +1,7 @@
 package com.dosug.app.services.authentication;
 
 import com.dosug.app.domain.AuthToken;
+import com.dosug.app.domain.User;
 
 /**
  * Created by radmir on 24.03.17.
@@ -9,4 +10,10 @@ public interface AuthenticationService {
 
     AuthToken login(String username, String password);
 
+    /**
+     * идентификация пользователя
+     * @param authKey ключ аутентификации
+     * @return null если таких нет
+     */
+    User authenticate(String authKey);
 }
