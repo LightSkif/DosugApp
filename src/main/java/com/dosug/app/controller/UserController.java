9e64732781bcb7ee5add861da34261bd40fab53e
LightSkif <LightSkif@yandex.ru>
2017-05-17 00:11:56 +0400

    Added user controller and get user id method.

---
+++ src/main/java/com/dosug/app/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.dosug.app.controller;
+
+import com.dosug.app.domain.User;
+import com.dosug.app.exception.NotAuthorizedException;
+import com.dosug.app.respose.model.Response;
+import com.dosug.app.services.authentication.AuthenticationService;
+import com.dosug.app.services.validation.ValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    private AuthenticationService authService;
+
+    private ValidationService validationService;
+
+    @GetMapping(value = "/get-id")
+    public Response getUserId(@RequestHeader(value = "authKey") String authKey) {
+
+        Response<Long> response = new Response<>();
+
+        User user = authService.authenticate(authKey);
+        if (user == null) {
+            throw new NotAuthorizedException();
+        }
+
+        return response.success(user.getId());
+    }
+
+    @Autowired
+    public void setAuthService(AuthenticationService authService) {
+        this.authService = authService;
+    }
+
+    @Autowired
+    public void setValidationService(ValidationService validationService) {
+        this.validationService = validationService;
+    }
+}
