237c163306a80e771047ee2af9b77b17ac287113
radmirnovii <radmirnovii@gmail.com>
2017-05-28 21:20:13 +0300

    add search by name queries

--- src/main/java/com/dosug/app/repository/EventRepository.java
+++ src/main/java/com/dosug/app/repository/EventRepository.java
@@ -4,13 +4,21 @@ import com.dosug.app.domain.Event;
 import com.dosug.app.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 
-public interface EventRepository extends CrudRepository<Event, String> {
+public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
+
+    @Query("FROM Event as e WHERE e.eventName LIKE CONCAT('%', :part, '%')")
+    Page<Event> simpleSearch(@Param("part") String part, Pageable pageable);
+
+    @Query("SELECT COUNT(*) FROM Event as e WHERE e.eventName LIKE CONCAT('%', :part, '%')")
+    long simpleSearchCount(@Param("part") String part);
 
     Event findById(long eventId);
 
