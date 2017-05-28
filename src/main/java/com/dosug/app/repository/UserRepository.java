4a6425bcfed56d4470704f249be643db3967b168
radmirnovii <radmirnovii@gmail.com>
2017-05-28 21:20:35 +0300

    add simple search by name email

--- src/main/java/com/dosug/app/repository/UserRepository.java
+++ src/main/java/com/dosug/app/repository/UserRepository.java
@@ -1,13 +1,20 @@
 package com.dosug.app.repository;
 
 import com.dosug.app.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by radmir on 23.03.17.
  */
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+    Page<User> simpleSearch(@Param("part") String part, Pageable pageable);
+
+    long simpleSearchCount(@Param("part") String part);
 
     User findById(Long id);
 
