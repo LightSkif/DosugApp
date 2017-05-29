aaef88c7282d7f893de9365c7b2eb04f81279b0e
radmirnovii <radmirnovii@gmail.com>
2017-05-29 16:32:34 +0300

    add ISO deserializer

---
+++ src/main/java/com/dosug/app/utils/ISOLocalDateDeserializer.java
@@ -0,0 +1,21 @@
+package com.dosug.app.utils;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Created by radmir on 28.05.17.
+ */
+public class ISOLocalDateDeserializer extends JsonDeserializer<LocalDate> {
+    private final static DateTimeFormatter format = DateTimeFormatter.ISO_LOCAL_DATE;
+
+    @Override
+    public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        return LocalDate.parse(jsonParser.getText(), format);
+    }
+}
