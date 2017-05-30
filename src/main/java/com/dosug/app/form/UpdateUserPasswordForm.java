bccb966641595e4018214750e1904cd85e78363f
radmirnovii <radmirnovii@gmail.com>
2017-05-31 00:10:13 +0300

    change new password messages

--- src/main/java/com/dosug/app/form/UpdateUserPasswordForm.java
+++ src/main/java/com/dosug/app/form/UpdateUserPasswordForm.java
@@ -21,7 +21,7 @@ public class UpdateUserPasswordForm {
 
     @ErrorCode(code = ApiErrorCode.INVALID_NEW_PASSWORD)
     @NotNull(message = "new_password_required")
-    @Size(min = Consts.PASSWORD_MIN_SYMBOLS, max = Consts.PASSWORD_MAX_SYMBOLS, message = "new_password_length_1_256")
+    @Size(min = Consts.PASSWORD_MIN_SYMBOLS, max = Consts.PASSWORD_MAX_SYMBOLS, message = "new_password_length_8_256")
     @Pattern(regexp = "[a-zA-Z0-9_]*", message = "new_password_allow_latinchar_digits_underscore")
     private String newPassword;
 
